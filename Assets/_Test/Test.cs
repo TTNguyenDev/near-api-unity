1285f3ef6b17c5e1211bb4357584b3e8da0139b4
vbph <hungvo.05122002.4@gmail.com>
2022-11-30 14:10:50 +0700

    feat: add test button

--- Assets/_Test/Test.cs
+++ Assets/_Test/Test.cs
@@ -1,9 +1,22 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Test : MonoBehaviour
 {
+    [SerializeField] private Button _btn;
+
+    private void Awake()
+    {
+        _btn.onClick.AddListener(OnButton);
+    }
+
     private void Start()
     {
         Debug.Log("We all love you very much, Neil. Sometimes not.");
+    }
+
+    private void OnButton()
+    {
+        Debug.Log("Button is clicked!");
     }
 }
