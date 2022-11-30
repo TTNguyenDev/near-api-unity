0140313756b944db33c25b56eeb61eda354ca375
vbph <hungvo.05122002.4@gmail.com>
2022-11-30 20:13:23 +0700

    feat: setup to build test

--- Assets/_Test/Test.cs
+++ Assets/_Test/Test.cs
@@ -1,9 +1,13 @@
+using System.Text;
+using NearClientUnity.Utilities.Ed25519;
+using Newtonsoft.Json;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Test : MonoBehaviour
 {
     [SerializeField] private Button _btn;
+    [SerializeField] private Text _log;
 
     private void Awake()
     {
@@ -12,11 +16,28 @@ public class Test : MonoBehaviour
 
     private void Start()
     {
-        Debug.Log("We all love you very much, Neil. Sometimes not.");
+        Log("We all love you very much, Neil. Sometimes not.");
+
+        try
+        {
+            var str = "Ayo, what's up bro?";
+            var key = Ed25519.ExpandedPrivateKeyFromSeed(Encoding.ASCII.GetBytes(str));
+            Log(JsonConvert.SerializeObject(key));
+        }
+        catch (System.Exception err)
+        {
+            Log(JsonConvert.SerializeObject(err));
+        }
     }
 
     private void OnButton()
     {
-        Debug.Log("Button is clicked!");
+        Log("Button is clicked!");
+    }
+
+    private void Log(string log)
+    {
+        _log.text = log;
+        Debug.Log(log);
     }
 }
